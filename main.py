c356a9823534c99db64194ef78879bd5049d7092
peshmerge <pesho.morad@gmail.com>
2017-12-07 16:23:47 +0100

    The latste function which answers the question number 2

--- main.py
+++ main.py
@@ -21,12 +21,91 @@ def main():
     print("Flex sensor#1= ", corrected_times[0])
     print("Flex sensor#2= ", corrected_times[1])
     print("Flex sensor#3= ", corrected_times[2])
+
     print(" The user has maintained the correct posture for ", correct_posture_duration)
     """
     @Todo should be done correctly after having real data.
     """
-    print("The percentage of total time is : ",correct_posture_duration / data[1][-1][0])
+    print("The percentage of total time is : ", correct_posture_duration / data[1][-1][0])
 
+    print("Average time needed before the user corrected himself per flex sensor")
+    print(get_time_before_correction(data[1], data[0]))
+
+
+# How long did it take before the user corrected him/herself (average of values)?
+def get_time_before_correction(data_list, calibration_values):
+    started = True
+    correction_flex_sensor_1 = [False, False]
+    correction_flex_sensor_2 = [False, False]
+    correction_flex_sensor_3 = [False, False]
+
+    time_list_1 = []
+    time_list_2 = []
+    time_list_3 = []
+    # Loop through the data list to be able to collect the needed data
+    data_list_counter = 1
+    for item in data_list:
+        if data_list_counter == len(data_list):
+            if correction_flex_sensor_1[0]:
+                time_list_1[-1] = [time_list_1[-1][0], item[0], item[0] - time_list_1[-1][0]]
+
+            if correction_flex_sensor_2[0]:
+                time_list_2[-1] = [time_list_2[-1][0], item[0], item[0] - time_list_2[-1][0]]
+
+            if correction_flex_sensor_3[0]:
+                time_list_3[-1] = [time_list_3[-1][0], item[0], item[0] - time_list_3[-1][0]]
+        else:
+            if item[1] < calibration_values[0]:
+                if correction_flex_sensor_1[0] == False:
+                    time_list_1.append([item[0], 0, 0])
+                    correction_flex_sensor_1 = [True, False]
+
+            else:
+                if correction_flex_sensor_1[0]:
+                    correction_flex_sensor_1 = [False, False]
+                    time_list_1[-1] = [time_list_1[-1][0], item[0], item[0] - time_list_1[-1][0]]
+
+            if item[2] < calibration_values[1]:
+                if correction_flex_sensor_2[0] == False:
+                    time_list_2.append([item[0], 0, 0])
+                    correction_flex_sensor_2 = [True, False]
+
+                else:
+                    if correction_flex_sensor_2[0]:
+                        correction_flex_sensor_2 = [False, False]
+                        time_list_2[-1] = [time_list_2[-1][0], item[0], item[0] - time_list_2[-1][0]]
+
+            if item[3] < calibration_values[2]:
+                if correction_flex_sensor_3[0] == False:
+                    time_list_3.append([item[0], 0, 0])
+                    correction_flex_sensor_3 = [True, False]
+
+                else:
+                    if correction_flex_sensor_3[0]:
+                        correction_flex_sensor_3 = [False, False]
+                        time_list_3[-1] = [time_list_3[-1][0], item[0], item[0] - time_list_3[-1][0]]
+            data_list_counter += 1
+
+    time_sum_1 = 0
+    time_sum_2 = 0
+    time_sum_3 = 0
+    for time_item in time_list_1:
+        time_sum_1 += time_item[2]
+
+    for time_item in time_list_2:
+        time_sum_2 += time_item[2]
+
+    for time_item in time_list_3:
+        time_sum_3 += time_item[2]
+
+    return [
+        time_sum_1 / len(time_list_1) if len(time_list_1) else 0,
+        time_sum_2 / len(time_list_2) if len(time_list_2) else 0,
+        time_sum_3 / len(time_list_3) if len(time_list_3) else 0
+        ]
+
+
+# How long did they maintain correct posture? (percentage of total time)
 def get_time_of_maintaining_correct_posture(data_list, calibration_values):
     """
     :param data_list
@@ -41,10 +120,9 @@ def get_time_of_maintaining_correct_posture(data_list, calibration_values):
 
     correct_posture_duration = 0
 
-    time_list = [[]]
+    time_list = []
 
     # Loop through the data list to be able to collect the needed data
-    time_list.pop()
     data_list_counter = 1
     for item in data_list:
         if data_list_counter == len(data_list):
@@ -65,8 +143,8 @@ def get_time_of_maintaining_correct_posture(data_list, calibration_values):
             data_list_counter += 1
 
     # Calculate the time (duration of how long the user maintained a correct posture)
-    for item in time_list:
-        correct_posture_duration = correct_posture_duration + item[2]
+    for time_item in time_list:
+        correct_posture_duration = correct_posture_duration + time_item[2]
     return correct_posture_duration
 
 
